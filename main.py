ce12e6fbf9c3d9061fba9eee814571134867ce1a
fengliang <feng.liang@kaust.edu.sa>
2023-05-19 09:59:46 +0000

    MVP

--- main.py
+++ main.py
@@ -3,30 +3,19 @@ from newletter_gpt.prompts import gen_summary_via_llm, get_tags_via_llm
 from datetime import datetime, timedelta
 import random
 import time
+import logging
 
-
-def test():
-    feed_sources = {
-        "机器之心": FeedSource("机器之心", "https://www.jiqizhixin.com/rss"),
-        "量子位": FeedSource("量子位", "https://www.qbitai.com/rss"),
-        "新智元": FeedSource("新智元", "https://feed.hamibot.com/api/feeds/61aa18e9486e3727fb090ba1"),
-    }
-    jiqizhixin = feed_sources["机器之心"]
-    feed = jiqizhixin.get_feeds()
-    print(feed)
-    pass
+logger = logging.getLogger("NewsletterGPT")
 
 
 if __name__ == "__main__":
-    test()
-
     feed_sources = {
         "机器之心": FeedSource("机器之心", "https://www.jiqizhixin.com/rss"),
         "量子位": FeedSource("量子位", "https://www.qbitai.com/rss"),
         "新智元": FeedSource("新智元", "https://feed.hamibot.com/api/feeds/61aa18e9486e3727fb090ba1"),
     }
-    last_update_time = datetime.now()
-    start_time = datetime.fromisoformat("Mon, 15 May 2023 08:00:00 +0800")
+    # start_time = datetime.fromisoformat("Mon, 15 May 2023 08:00:00 +0800")
+    start_time = datetime.now()
     one_week = timedelta(days=7)
     week_end = start_time + one_week
     feeds_this_week = set()
@@ -35,17 +24,21 @@ if __name__ == "__main__":
         for feed_name, feed_source in feed_sources.items():
             feed_items, is_updated, new_items = feed_source.get_feeds()
             if is_updated:
-                print(f"New entries from {feed_name}:")
+                # save relevant feeds
                 for item in new_items:
-                    print(f"{item.title}: {item.link}")
-                    feeds_this_week.add(item)
+                    gen_summary_via_llm(item)
+                    get_tags_via_llm(item)
+                    if item.is_relevant():
+                        logger.info(f" New entries from {feed_name} --  {item.title}: {item.link}")
+                        feeds_this_week.add(item)
 
+        # if one week has passed, print related feeds of last week
         if datetime.now() > week_end:
-            print("Sending newsletter")
-            print(f"Feeds this week: {feeds_this_week}")
+            logger.info(f"Feeds this week: {[feed.to_json() for feed in feeds_this_week]}")
             feeds_this_week = set()
             week_end = datetime.now() + one_week
+
         # sleep for a random time between 12 and 24 hours
         sleep_time = random.randint(12, 24) * 60 * 60
-        print(f"Sleeping for {sleep_time} seconds")
+        logger.info(f"Sleeping for {sleep_time / (60*60)} hours and then check new update.")
         time.sleep(sleep_time)
